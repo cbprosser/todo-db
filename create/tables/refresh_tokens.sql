4ef058bb27a8908941d0ad5c476a3ba8536b88e4
C. B. Prosser <52571711+cbprosser@users.noreply.github.com>
2024-03-23 02:10:09 -0600

    Moving to fingerprint for more consistency

--- create/tables/refresh_tokens.sql
+++ create/tables/refresh_tokens.sql
@@ -7,10 +7,9 @@ CREATE TABLE IF NOT EXISTS public.refresh_tokens
     refresh_token_id uuid NOT NULL DEFAULT uuid_generate_v4(),
     token character varying COLLATE pg_catalog."default" NOT NULL,
     expire_date timestamp without time zone NOT NULL DEFAULT (CURRENT_DATE + '30 days'::interval),
-    device text COLLATE pg_catalog."default" NOT NULL,
-    address text COLLATE pg_catalog."default" NOT NULL,
+    fingerprint character varying COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT "refreshTokens_pkey" PRIMARY KEY (refresh_token_id),
-    CONSTRAINT device_address_unique UNIQUE (device, address)
+    CONSTRAINT unique_fingerprint_constraint UNIQUE (fingerprint)
 )
 
 TABLESPACE pg_default;
